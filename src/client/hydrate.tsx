ad0e251636d5e754c3e0d61c9aee5e85199d1de2
Justin <shpaw415@gmail.com>
2025-10-23 01:17:39 -0400

    fix-react JSXDEV in production

--- src/client/hydrate.tsx
+++ src/client/hydrate.tsx
@@ -10,9 +10,65 @@ import Shell from "../Shell";
 import type { JSX } from "react";
 import { join } from "../utils";
 import { layoutGetter, StackLayouts } from "../router/layout";
-import { fixReact } from "./fix-react";
+import { jsxDEV, type JSXSource } from "react/jsx-dev-runtime";
+import { jsxs } from "react/jsx-runtime";
+import React from "react";
 
-fixReact();
+declare global {
+  var jsx_w77yafs4: (
+    type: React.ElementType,
+    props: unknown,
+    key?: React.Key
+  ) => React.ReactElement;
+  var jsx: (
+    type: React.ElementType,
+    props: unknown,
+    key?: React.Key
+  ) => React.ReactElement;
+  var jsxDEV_7x81h0kn: (
+    type: React.ElementType,
+    props: unknown,
+    key: React.Key | undefined,
+    isStatic: boolean,
+    source?: JSXSource,
+    self?: unknown
+  ) => React.ReactElement;
+  var jsxDEV: (
+    type: React.ElementType,
+    props: unknown,
+    key: React.Key | undefined,
+    isStatic: boolean,
+    source?: JSXSource,
+    self?: unknown
+  ) => React.ReactElement;
+  //@ts-ignore
+  var jsxs_eh6c78nj: (
+    type: React.ElementType,
+    props: unknown,
+    key?: React.Key
+  ) => React.ReactElement;
+  var jsxs: (
+    type: React.ElementType,
+    props: unknown,
+    key?: React.Key
+  ) => React.ReactElement;
+  var Fragment_8vg9x3sq: React.ExoticComponent<{
+    children?: React.ReactNode | undefined;
+  }>;
+  var Fragment: React.ExoticComponent<{
+    children?: React.ReactNode | undefined;
+  }>;
+}
+
+globalThis.React = React;
+globalThis.jsx_w77yafs4 = jsx;
+globalThis.jsx = jsx;
+globalThis.jsxDEV_7x81h0kn = jsxDEV;
+globalThis.jsxDEV = jsxDEV;
+globalThis.jsxs_eh6c78nj = jsxs;
+globalThis.jsxs = jsxs;
+globalThis.Fragment_8vg9x3sq = Fragment;
+globalThis.Fragment = Fragment;
 
 async function start() {
   const url = new URL(window.location.href);
